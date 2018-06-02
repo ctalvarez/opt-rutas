65df1fe4171ab6332c129b8872137a8710298dee
ctalvarez <ctalvarez@uc.cl>
2018-06-02 15:48:52 -0400

    added demand

--- generacion_ventanas.py
+++ generacion_ventanas.py
@@ -41,12 +41,54 @@ tamano_anden = df_locales['TamanoAnden']
 
 # obtener demandas
 df_demandas = pd.read_excel('VIII-distancias-duraciones2.xlsx', sheet_name='demandas')
-demands = [
-    df_demandas['Lunes'],
-    df_demandas['Martes'],
-    df_demandas['Miercoles'],
-    df_demandas['Jueves'],
-    df_demandas['Viernes'],
-    df_demandas['Sabado']
+
+days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+averages = ['prom-lunes', 'prom-martes', 'prom-miercoles', 'prom-jueves', 'prom-viernes', 'prom-sabado']
+stds = ['std-lunes', 'std-martes', 'std-miercoles', 'std-jueves', 'std-viernes', 'std-sabado']
+maxs = ['max-lunes', 'max-martes', 'max-miercoles', 'max-jueves', 'max-viernes', 'max-sabado']
+
+inconsitencias = 0
+
+for i in range(len(df_demandas)):
+    for k in range(len(averages)):
+        if type(df_locales[days[k]].iloc[i]) == list:
+            if pd.notna(df_locales[days[k]].iloc[i][0]) and pd.notna(df_demandas[averages[k]].iloc[i]):
+                continue
+            elif (pd.notna(df_locales[days[k]].iloc[i][0]) or pd.notna(df_demandas[averages[k]].iloc[i])):
+                print('index:', i)
+                print('local:', df_locales['Nombre'][i])
+                print('dia:', days[k], 'ventana:', df_locales[days[k]].iloc[i])
+                print(df_demandas[averages[k]].iloc[i])
+                print('-'*50)
+                inconsitencias += 1
+        else:
+            if pd.notna(df_locales[days[k]].iloc[i]) and pd.notna(df_demandas[averages[k]].iloc[i]):
+                continue
+            elif (pd.notna(df_locales[days[k]].iloc[i]) or pd.notna(df_demandas[averages[k]].iloc[i])):
+                print('index:', i)
+                print('local:', df_locales['Nombre'][i])
+                print('dia:', days[k], 'ventana:', df_locales[days[k]].iloc[i])
+                print(df_demandas[averages[k]].iloc[i])
+                print('-'*50)
+                inconsitencias += 1
+print('inconsitencias:', inconsitencias)
+
+demands_average = [
+    df_demandas['prom-lunes'],
+    df_demandas['prom-martes'],
+    df_demandas['prom-miercoles'],
+    df_demandas['prom-jueves'],
+    df_demandas['prom-viernes'],
+    df_demandas['prom-sabado']
+]
+demands_std = [
+    df_demandas['std-lunes'],
+    df_demandas['std-martes'],
+    df_demandas['std-miercoles'],
+    df_demandas['std-jueves'],
+    df_demandas['std-viernes'],
+    df_demandas['std-sabado']
 ]
 
+del df_demandas
+del df_locales
